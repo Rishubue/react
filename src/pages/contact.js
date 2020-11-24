388debb64a4b175cc049869f0c8d923719861929
johan031 <jovantanaskovic97@gmail.com>
2020-11-24 21:49:28 +0100

    sredjen dizajn brapooo

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,10 +1,12 @@
 import React from "react"
 import Layout from "../components/Layouts/LayoutContact"
 import Contact from "../components/Contact/Contact"
+import Info from "../components/Home/Info"
 
 const contact = () => {
   return (
     <Layout>
+      <Info />
       <Contact />
     </Layout>
   )
