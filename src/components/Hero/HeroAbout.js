a4065dcca38fb8cb22870ed9a89b5fb0a350ac02
johan031 <jovantanaskovic97@gmail.com>
2020-12-07 13:54:28 +0100

    background img na ostalim stranama

--- src/components/Hero/HeroAbout.js
+++ src/components/Hero/HeroAbout.js
@@ -1,21 +1,46 @@
 import React, { useState } from "react"
 import Navbar from "../Navbar"
 import Sidebar from "../Sidebar"
+import BackgroundImage from "gatsby-background-image"
+import { useStaticQuery, graphql } from "gatsby"
+
+const query = graphql`
+  {
+    file(relativePath: { eq: "salad.jpg" }) {
+      childImageSharp {
+        fluid {
+          ...GatsbyImageSharpFluid
+        }
+      }
+    }
+  }
+`
 
 const HeroAbout = () => {
+  const {
+    file: {
+      childImageSharp: { fluid },
+    },
+  } = useStaticQuery(query)
+
   const [isOpen, setIsOpen] = useState(false)
 
   const toggleNav = () => {
     setIsOpen(!isOpen)
   }
   return (
-    <header className="header-about">
+    <BackgroundImage
+      Tag="header"
+      className="header-about"
+      fluid={fluid}
+      preserveStackingContext={true}
+    >
       <Navbar toggleNav={toggleNav} />
       <Sidebar isOpen={isOpen} toggleNav={toggleNav} />
       <div className="header-about__text-box">
         <h1 className="logo-text margin-bottom-smallest">ovo je nasa prica</h1>
       </div>
-    </header>
+    </BackgroundImage>
   )
 }
 
--- src/components/Hero/HeroContact.js
+++ src/components/Hero/HeroContact.js
@@ -1,21 +1,46 @@
 import React, { useState } from "react"
 import Navbar from "../Navbar"
 import Sidebar from "../Sidebar"
+import BackgroundImage from "gatsby-background-image"
+import { useStaticQuery, graphql } from "gatsby"
+
+const query = graphql`
+  {
+    file(relativePath: { eq: "hero.jpg" }) {
+      childImageSharp {
+        fluid {
+          ...GatsbyImageSharpFluid
+        }
+      }
+    }
+  }
+`
 
 const HeroContact = () => {
+  const {
+    file: {
+      childImageSharp: { fluid },
+    },
+  } = useStaticQuery(query)
+
   const [isOpen, setIsOpen] = useState(false)
 
   const toggleNav = () => {
     setIsOpen(!isOpen)
   }
   return (
-    <header className="header-contact">
+    <BackgroundImage
+      Tag="header"
+      fluid={fluid}
+      preserveStackingContext={true}
+      className="header-contact"
+    >
       <Navbar toggleNav={toggleNav} />
       <Sidebar isOpen={isOpen} toggleNav={toggleNav} />
-      <div className="header-contact__text-box">
-        <h1 className="logo-text margin-bottom-smallest">Kontakt</h1>
+      <div className="header-gallery__text-box">
+        <h1 className="logo-text margin-bottom-smallest">kontakt</h1>
       </div>
-    </header>
+    </BackgroundImage>
   )
 }
 
